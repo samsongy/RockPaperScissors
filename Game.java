702b2a347f28cb391abcae31b066c6d1ac607f06
Sam Songy <60078148+samsongy@users.noreply.github.com>
2020-05-03 16:07:23 -0500

    Created welcome prompt before beginning the game

--- Game.java
+++ Game.java
@@ -1,6 +1,8 @@
 public class Game {
 
     public static void main(String[] args) {
-        System.out.println("test");
+
+
+        System.out.println("\nWelcome to Rock Paper Scissors");
     }
 }
