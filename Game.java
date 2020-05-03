822f1a116773ad61219be2cee1a7e9c38cceded7
Sam Songy <60078148+samsongy@users.noreply.github.com>
2020-05-03 16:46:25 -0500

    Created while loop to continue playing game, added scanner object to see whether user wants to continue playing, added conditional flow to make decision on whether or not to continue based on the users input

--- Game.java
+++ Game.java
@@ -1,8 +1,53 @@
+import java.util.Scanner;
+
+
 public class Game {
 
     public static void main(String[] args) {
 
+        System.out.println("\nWelcome to Rock Paper Scissors\n");
 
-        System.out.println("\nWelcome to Rock Paper Scissors");
+        boolean contineGame = true;
+
+        while(contineGame){
+
+            char userAnswer;
+            Scanner sc = new Scanner(System.in);
+
+
+            System.out.println("\nWould you like to keep playing? (y/n): ");
+            userAnswer = sc.next().charAt(0);
+
+            
+                 
+            if(userAnswer == 'y'){
+                
+                contineGame = true;
+
+            } else if(userAnswer == 'n') {
+                
+                contineGame = false;
+
+            } else {
+                while(userAnswer != 'y' && userAnswer != 'n') {
+
+                    System.out.println("\nPlease choose either y or n");
+                    userAnswer = sc.next().charAt(0);
+
+                    if(userAnswer == 'y'){
+                        contineGame = true;
+                    }
+
+                    if(userAnswer == 'n'){
+                        contineGame = false;
+                    }
+                }
+            }
+
+
+
+        }
+
+
     }
 }
