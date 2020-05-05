0be50bdfc93db73ff5f45dcac04a55aba920825b
Sam Songy <60078148+samsongy@users.noreply.github.com>
2020-05-05 12:30:31 -0500

    Added ComputerMove Method that generates a random number between 0 and 2 and assigns a move based on the number generated

--- Game.java
+++ Game.java
@@ -1,7 +1,30 @@
 import java.util.Scanner;
-
+import java.util.Random;
 
 public class Game {
+
+    public static String ComputerMove(){
+
+        Random rand = new Random();
+        String computerMove = null;
+        int generatedNumber = rand.nextInt(3);
+
+        switch (generatedNumber) {
+            case 0:
+                computerMove = "rock";
+                break;
+            case 1:
+                computerMove = "paper";
+                break;
+            case 2:
+                computerMove = "scissors";
+                break;
+        }
+
+        return computerMove;
+    }
+
+    
 
     public static void main(String[] args) {
 
@@ -15,29 +38,29 @@ public class Game {
             Scanner sc = new Scanner(System.in);
 
 
+            //testing to see if ComputerMove method is working
+            System.out.println("\nComputer Move: " + ComputerMove());
+
+
+
+
             System.out.println("\nWould you like to keep playing? (y/n): ");
             userAnswer = sc.next().charAt(0);
 
             
                  
             if(userAnswer == 'y'){
-                
                 contineGame = true;
-
             } else if(userAnswer == 'n') {
-                
                 contineGame = false;
-
             } else {
                 while(userAnswer != 'y' && userAnswer != 'n') {
-
                     System.out.println("\nPlease choose either y or n");
                     userAnswer = sc.next().charAt(0);
 
                     if(userAnswer == 'y'){
                         contineGame = true;
                     }
-
                     if(userAnswer == 'n'){
                         contineGame = false;
                     }
